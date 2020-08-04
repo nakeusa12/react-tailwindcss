4ee675172a2015c6baf6cce2bbab1b73ec350a89
Saiful Nakeusa <snakeusa8@gmail.com>
2020-08-04 23:37:30 +0700

    ambil api pixabay

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
+import ImageCard from './components/ImageCard';
+
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState('');
+
+
+  useEffect(() => {
+    fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
+      .then(res => res.json())
+      .then(data =>  {
+        setImages(data.this);
+        setIsLoading(false);
+      })
+      .catch(err => console.log(err))
+  }, []);
+
   return (
-    <div>
-      <h1 className="text-3xl text-red-500">Haloo</h1>
-    </div>
+    <ImageCard />
   );
 }
 
