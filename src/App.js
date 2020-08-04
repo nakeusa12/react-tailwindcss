898f4d4989ed8bb8de574eee2851510bd59a88dd
Saiful Nakeusa <snakeusa8@gmail.com>
2020-08-04 23:48:40 +0700

    menampilkan data api

--- src/App.js
+++ src/App.js
@@ -12,14 +12,21 @@ function App() {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())
       .then(data =>  {
-        setImages(data.this);
+        setImages(data.hits);
         setIsLoading(false);
       })
       .catch(err => console.log(err))
   }, []);
 
   return (
-    <ImageCard />
+    <div className="container mx-auto">
+      {isLoading ? <h1 className="text-6xl text-center mx-auto mt-32">Loading...</h1> :  <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
+      </div>}
+    </div>
+
   );
 }
 
